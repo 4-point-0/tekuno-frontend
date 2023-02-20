87b17ad83f29c743c686911c0660aa50d2c659db
badurina <antun.badurina@gmail.com>
2023-02-20 16:20:31 +0100

    Simplify QR claim URL

--- utils/qrcode.ts
+++ utils/qrcode.ts
@@ -1,6 +1,3 @@
 export const getClaimURL = (nftId: string) => {
-  const vercelUrl = process.env.NEXT_PUBLIC_VERCEL_URL;
-  const url = vercelUrl ? `https://${vercelUrl}` : "http://localhost:3000";
-
-  return `${url}/claim/${nftId}`;
+  return `${location.origin}/claim/${nftId}`;
 };
