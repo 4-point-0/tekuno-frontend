949f05f4421f96bf14f83bceba3f37d5c5df24e4
badurina <antun.badurina@gmail.com>
2023-03-01 14:35:51 +0100

    Fix login page glitch

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,11 +1,16 @@
 import { useRamper } from "@/context/RamperContext";
-import { Box, Button, Container, Image, Stack } from "@mantine/core";
-import { useMediaQuery } from "@mantine/hooks";
+import {
+  Box,
+  Button,
+  Container,
+  Image,
+  MediaQuery,
+  Stack,
+} from "@mantine/core";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function Login() {
-  const isBigScreen = useMediaQuery("(min-width: 900px)");
   const { signIn, loading, user } = useRamper();
   const router = useRouter();
 
@@ -23,36 +28,53 @@ export default function Login() {
 
   return (
     <Container>
-      <Stack align={"center"}>
-        <Box
-          sx={{
-            width: isBigScreen ? "50vw" : "100vw",
-          }}
-        >
-          <Image
-            fit="contain"
-            src="/login-gradient.svg"
-            alt="Half circle with gradient"
-          />
-        </Box>
-        <Image
-          mt={isBigScreen ? 80 : 40}
-          src="/tekuno.svg"
-          width={300}
-          alt="Tekuno logo"
-        />
-        <Button
-          loading={loading}
-          mt={"lg"}
-          variant="filled"
-          color="dark"
-          radius={"xl"}
-          size="lg"
-          onClick={signIn}
-        >
-          Connect to Tekuno
-        </Button>
-      </Stack>
+      <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+        <Stack align={"center"}>
+          <Box
+            sx={{
+              width: "100vw",
+            }}
+          >
+            <Image fit="contain" src="/login-gradient.svg" alt="" />
+          </Box>
+          <Image mt={40} src="/tekuno.svg" width={300} alt="Tekuno logo" />
+          <Button
+            loading={loading}
+            mt={"lg"}
+            variant="filled"
+            color="dark"
+            radius={"xl"}
+            size="lg"
+            onClick={signIn}
+          >
+            Connect to Tekuno
+          </Button>
+        </Stack>
+      </MediaQuery>
+
+      <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
+        <Stack align={"center"}>
+          <Box
+            sx={{
+              width: "50vw",
+            }}
+          >
+            <Image fit="contain" src="/login-gradient.svg" alt="" />
+          </Box>
+          <Image mt={80} src="/tekuno.svg" width={300} alt="Tekuno logo" />
+          <Button
+            loading={loading}
+            mt={"lg"}
+            variant="filled"
+            color="dark"
+            radius={"xl"}
+            size="lg"
+            onClick={signIn}
+          >
+            Connect to Tekuno
+          </Button>
+        </Stack>
+      </MediaQuery>
     </Container>
   );
 }
