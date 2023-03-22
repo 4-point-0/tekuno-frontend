e947807daf0473855c5fff8f6bc2923ff9b73484
badurina <antun.badurina@gmail.com>
2023-03-22 11:27:47 +0100

    Fix campaign details loading state

--- components/admin/CampaignDetails/index.tsx
+++ components/admin/CampaignDetails/index.tsx
@@ -6,7 +6,7 @@ import {
   Grid,
   Group,
   Image,
-  SimpleGrid,
+  Paper,
   Skeleton,
   Stack,
   Text,
@@ -41,20 +41,7 @@ import { notifications } from "@/utils/notifications";
 import { CampaignStatus } from "../CampaignStatus";
 import { DownloadAll } from "./DownloadAll";
 import { QRPreview } from "./QRPreview";
-import { StatBox } from "./StatBox";
 import { StatusButtons } from "./StatusButtons";
-
-const stats = [
-  {
-    label: "NFTs CLAIMED",
-    value: 1342,
-    subLabel: "In Total",
-  },
-  {
-    label: "TOTAL SUPPLY",
-    value: 10000,
-  },
-];
 
 export const CampaignDetails = () => {
   const router = useRouter();
@@ -89,90 +76,101 @@ export const CampaignDetails = () => {
     }
   };
 
+  if (isLoading) {
+    return null;
+  }
+
   return (
-    <Container>
-      <Stack>
-        <Skeleton visible={isLoading}>
-          <Box>
-            <Image src={getImageUrl(image)} radius="lg" alt="" />
-          </Box>
-        </Skeleton>
+    <Container fluid>
+      <Paper radius="lg" p="xl">
+        <Container>
+          {campaign && (
+            <Stack>
+              <Skeleton
+                visible={isLoading}
+                w="100%"
+                radius="lg"
+                style={{ aspectRatio: 3 / 1 }}
+              >
+                <Box>
+                  <Image src={getImageUrl(image)} radius="lg" alt="" />
+                </Box>
+              </Skeleton>
 
-        <Box>
-          <Skeleton visible={isLoading}>
-            <Group>
-              {campaign && (
-                <CampaignStatus status={campaign?.status} size="lg" />
+              <Box>
+                <Group>
+                  {campaign && (
+                    <CampaignStatus status={campaign?.status} size="lg" />
+                  )}
+                  <Title order={2}>{campaign?.name}</Title>
+                </Group>
+                <Text c="dimmed">
+                  {formatDateRange(
+                    campaign?.start_date as string,
+                    campaign?.end_date
+                  )}
+                </Text>
+              </Box>
+
+              <Group position="right">
+                {campaign?.status === "Created" && !isEnded && (
+                  <>
+                    <IndigoButton
+                      component="a"
+                      href={`${location.origin}/admin/previous/${router.query.id}/preview`}
+                      target="_blank"
+                      rel="norefferer"
+                      leftIcon={<Eye size={14} />}
+                    >
+                      Preview
+                    </IndigoButton>
+                    <Button
+                      component={Link}
+                      href={`/admin/previous/${router.query.id}/edit`}
+                      leftIcon={<Pencil size={14} />}
+                      color="dark"
+                    >
+                      Edit
+                    </Button>
+                  </>
+                )}
+                {campaign?.status !== "Created" && (
+                  <>
+                    <IndigoButton
+                      loaderPosition="right"
+                      loading={downloadingReport}
+                      rightIcon={<Report size={14} />}
+                      onClick={handleReportDownload}
+                    >
+                      Download report
+                    </IndigoButton>
+                    <IndigoButton
+                      component="a"
+                      href={`${location.origin}/campaign/${router.query.id}`}
+                      target="_blank"
+                      rel="norefferer"
+                      referrerPolicy="no-referrer"
+                      leftIcon={<ExternalLink size={14} />}
+                    >
+                      Open campaign
+                    </IndigoButton>
+                  </>
+                )}
+
+                {campaign && <StatusButtons campaign={campaign} />}
+              </Group>
+
+              {campaign?.description && (
+                <FormattedHTML fz="lg" content={campaign.description} />
               )}
-              <Title order={2}>{campaign?.name}</Title>
-            </Group>
-          </Skeleton>
-          <Skeleton visible={isLoading}>
-            <Text c="dimmed">
-              {formatDateRange(
-                campaign?.start_date as string,
-                campaign?.end_date
+              {campaign?.additional_description && (
+                <FormattedHTML
+                  fz="sm"
+                  content={campaign.additional_description}
+                />
               )}
-            </Text>
-          </Skeleton>
-        </Box>
 
-        <Group position="right">
-          {campaign?.status === "Created" && !isEnded && (
-            <>
-              <IndigoButton
-                component="a"
-                href={`${location.origin}/admin/previous/${router.query.id}/preview`}
-                target="_blank"
-                rel="norefferer"
-                leftIcon={<Eye size={14} />}
-              >
-                Preview
-              </IndigoButton>
-              <Button
-                component={Link}
-                href={`/admin/previous/${router.query.id}/edit`}
-                leftIcon={<Pencil size={14} />}
-                color="dark"
-              >
-                Edit
-              </Button>
-            </>
-          )}
-          {campaign?.status !== "Created" && (
-            <>
-              <IndigoButton
-                loaderPosition="right"
-                loading={downloadingReport}
-                rightIcon={<Report size={14} />}
-                onClick={handleReportDownload}
-              >
-                Download report
-              </IndigoButton>
-              <IndigoButton
-                component="a"
-                href={`${location.origin}/campaign/${router.query.id}`}
-                target="_blank"
-                rel="norefferer"
-                referrerPolicy="no-referrer"
-                leftIcon={<ExternalLink size={14} />}
-              >
-                Open campaign
-              </IndigoButton>
-            </>
-          )}
-
-          {campaign && <StatusButtons campaign={campaign} />}
-        </Group>
-
-        {campaign?.description && (
-          <FormattedHTML fz="lg" content={campaign.description} />
-        )}
-        {campaign?.additional_description && (
-          <FormattedHTML fz="sm" content={campaign.additional_description} />
-        )}
-
-        <SimpleGrid display="none" cols={4}>
+              {/* <SimpleGrid display="none" cols={4}>
           {stats.map((stat) => (
             <StatBox
               key={stat.label}
@@ -181,58 +179,61 @@ export const CampaignDetails = () => {
               subLabel={stat.subLabel}
             />
           ))}
-        </SimpleGrid>
-        {documents?.length !== 0 && (
-          <Group position="apart" align="flex-start" noWrap>
-            <Group miw={200}>
-              <Title order={4}>Project documents</Title>
-            </Group>
+        </SimpleGrid> */}
+              {documents?.length !== 0 && (
+                <Group position="apart" align="flex-start" noWrap>
+                  <Group miw={200}>
+                    <Title order={4}>Project documents</Title>
+                  </Group>
 
-            <Group position="right">
-              {documents?.map((document) => (
-                <DownloadBadge key={document.id} document={document} />
-              ))}
-            </Group>
-          </Group>
-        )}
+                  <Group position="right">
+                    {documents?.map((document) => (
+                      <DownloadBadge key={document.id} document={document} />
+                    ))}
+                  </Group>
+                </Group>
+              )}
 
-        <Group position="apart">
-          <Title order={4}>Digital collectibles</Title>
-          {campaign && (nfts?.length || 0) > 1 && (
-            <DownloadAll campaign={campaign} nfts={nfts as NftDto[]} />
+              <Group position="apart">
+                <Title order={4}>Digital collectibles</Title>
+                {campaign && (nfts?.length || 0) > 1 && (
+                  <DownloadAll campaign={campaign} nfts={nfts as NftDto[]} />
+                )}
+              </Group>
+
+              {!isEnded && campaign?.status === "Created" && (
+                <Alert
+                  icon={<AlertTriangle />}
+                  title="Campaign not started!"
+                  color="orange"
+                  radius="md"
+                >
+                  Start campaign so digital collectibles can be claimed.
+                </Alert>
+              )}
+
+              <Grid align="center">
+                {reward && (
+                  <Grid.Col span={12}>
+                    <NFTCard nft={reward} />
+                  </Grid.Col>
+                )}
+
+                {nfts?.map((nft) => (
+                  <Fragment key={nft.id}>
+                    <Grid.Col span={7}>
+                      <NFTCard key={nft.id} nft={nft} />
+                    </Grid.Col>
+                    <Grid.Col span={5}>
+                      <QRPreview nft={nft} />
+                    </Grid.Col>
+                  </Fragment>
+                ))}
+              </Grid>
+            </Stack>
           )}
-        </Group>
-
-        {!isEnded && campaign?.status === "Created" && (
-          <Alert
-            icon={<AlertTriangle />}
-            title="Campaign not started!"
-            color="orange"
-            radius="md"
-          >
-            Start campaign so digital collectibles can be claimed.
-          </Alert>
-        )}
-
-        <Grid align="center">
-          {reward && (
-            <Grid.Col span={12}>
-              <NFTCard nft={reward} />
-            </Grid.Col>
-          )}
-
-          {nfts?.map((nft) => (
-            <Fragment key={nft.id}>
-              <Grid.Col span={7}>
-                <NFTCard key={nft.id} nft={nft} />
-              </Grid.Col>
-              <Grid.Col span={5}>
-                <QRPreview nft={nft} />
-              </Grid.Col>
-            </Fragment>
-          ))}
-        </Grid>
-      </Stack>
+        </Container>
+      </Paper>
     </Container>
   );
 };
