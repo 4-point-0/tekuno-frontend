afdc75cf648d80dad49613c88eeffd29cfe6340b
badurina <antun.badurina@gmail.com>
2023-03-24 13:59:16 +0100

    Add initial sign in form

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,21 +1,64 @@
 import fetch from "isomorphic-fetch";
 import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 
+import {
+  fetchAuthControllerLogin,
+  fetchAuthControllerRegister,
+} from "@/services/api/admin/adminComponents";
 import { GoogleVerificationDto } from "@/services/api/admin/adminSchemas";
 
 export const authOptions: NextAuthOptions = {
+  pages: {
+    signIn: "/admin/auth/signin",
+  },
   providers: [
+    CredentialsProvider({
+      credentials: {},
+      async authorize(_, req) {
+        const { password, email, type, password_confirm } = req.body as any;
+
+        if (type === "register") {
+          try {
+            await fetchAuthControllerRegister({
+              body: {
+                email,
+                password,
+                password_confirm,
+              },
+            });
+          } catch (error) {
+            console.error(error);
+
+            return null;
+          }
+        }
+
+        try {
+          const jwt = await fetchAuthControllerLogin({
+            body: {
+              email,
+              password,
+            },
+          });
+
+          return { id: email, token: (jwt as any).token };
+        } catch (error) {
+          console.error(error);
+
+          return null;
+        }
+      },
+    }),
+
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
     }),
   ],
-  theme: {
-    colorScheme: "light",
-  },
   callbacks: {
-    async jwt({ token, account }) {
+    async jwt({ token, account, user }) {
       if (account?.access_token) {
         const res = await fetch(`${process.env.API_URL}/api/v1/google/auth`, {
           method: "POST",
@@ -29,6 +72,8 @@ export const authOptions: NextAuthOptions = {
         });
 
         token.res = (await res.json()) as GoogleVerificationDto;
+      } else if ((user as any)?.token) {
+        token.res = { token: (user as any).token };
       }
 
       return token;
