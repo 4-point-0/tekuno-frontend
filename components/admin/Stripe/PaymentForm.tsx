6c12bfab0b36b0b8d4d3be59f907659180d1fd76
Dominik Bosnjak <dominik.bosnjak94@gmail.com>
2023-06-13 13:32:04 +0200

    removing create-payment-intent

--- components/admin/Stripe/PaymentForm.tsx
+++ components/admin/Stripe/PaymentForm.tsx
@@ -193,9 +193,6 @@ export default function CheckoutForm() {
       return;
     }
 
-    console.log("paymentMethod", paymentMethod.id);
-    console.log("order", order?.order_id);
-
     // Create the PaymentIntent
     //TODO: Needs to be implemented in adminComponents file
     const res = await fetch(
