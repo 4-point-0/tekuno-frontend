552f77e86355251aaf406fe37dec45eba41fdfd0
Dominik Bosnjak <dominik.bosnjak94@gmail.com>
2023-06-13 16:13:03 +0200

    refactoring how stripe is loaded

--- pages/admin/stripe/payment/[id].tsx
+++ pages/admin/stripe/payment/[id].tsx
@@ -10,9 +10,7 @@ import { ClientContainer } from "../../../../components/layout/ClientContainer";
 // This is your test publishable API key.
 
 export default function Checkout() {
-  const stripePromise = loadStripe(
-    `pk_test_51MvyrIFQJt4X4BR30p2KmUzuQVOxsb8WZ1PdeA1NcoLWXMgcMiczx6uDnPACt7oL2YTDST2EOnOPPHOeDnTrZM52007EzP2nrq`
-  );
+  const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!);
 
   useEffect(() => {
     async function initializeStripe() {
