51af8c3974793572ffa4a6cc0d86a646a7bacb8e
dbosnjak94 <dominik.bosnjak94@gmail.com>
2023-08-21 16:00:40 +0200

    Tokens Page - refactor

--- pages/admin/tokens/index.tsx
+++ pages/admin/tokens/index.tsx
@@ -16,6 +16,7 @@ import {
 } from "@mantine/core";
 import { FaEuroSign } from "react-icons/fa";
 
+import PricingCards from "@/components/core/PricingCards";
 import {
   useStripeControllerGetProduct,
   useStripeControllerGetProducts,
@@ -48,7 +49,9 @@ export const Tokens = () => {
   const { classes } = useStyles();
   const SECONDARY_COL_HEIGHT = `calc(${PRIMARY_COL_HEIGHT} / 2 - ${theme.spacing.md} / 2)`;
 
-  // const data = useStripeControllerGetProducts();
+  const { data } = useStripeControllerGetProducts({});
+
+  console.log(data);
 
   return (
     <Box w="90%" mx="auto" h="100%" ta="center">
@@ -57,150 +60,19 @@ export const Tokens = () => {
       </Box>
       <Box mt="xl">
         <Title c="dimmed" fz={20}>
-          {`We've got the plan that is perfect for you}`}
+          {`We've got the plan that is perfect for you`}
         </Title>
       </Box>
       <SimpleGrid
         cols={3}
         spacing="md"
-        breakpoints={[{ maxWidth: "sm", cols: 1 }]}
+        breakpoints={[
+          { maxWidth: "sm", cols: 1 },
+          { maxWidth: "lg", cols: 2 },
+        ]}
         mt="xl"
       >
-        {/* <Skeleton height={PRIMARY_COL_HEIGHT} radius="md" animate={false} />
-        <Skeleton height={PRIMARY_COL_HEIGHT} radius="md" animate={false} />
-        <Skeleton height={PRIMARY_COL_HEIGHT} radius="md" animate={false} />
-        <Skeleton height={PRIMARY_COL_HEIGHT} radius="md" animate={false} /> */}
-
-        <Card
-          h={PRIMARY_COL_HEIGHT}
-          className={classes.container}
-          ta="center"
-          radius="lg"
-          px="auto"
-          py="auto"
-        >
-          <Box h="85%" mt="md">
-            <Title order={1}>Small</Title>
-
-            <Box mx="auto" w="30%" mt="md">
-              <Text fz={50} fw={700}>
-                10
-              </Text>
-              <Text fz={20} fw={700}>
-                euros
-              </Text>
-            </Box>
-            <Box w="30%" h="30%" mx="auto">
-              <Image src={"/images/small.png"}></Image>
-            </Box>
-
-            <Box mt="xl">
-              <Text c="dimmed">Enough for few small campaigns</Text>
-            </Box>
-          </Box>
-          <Button className={classes.button}>Order Now</Button>
-        </Card>
-        <Card
-          h={PRIMARY_COL_HEIGHT}
-          className={classes.container}
-          ta="center"
-          radius="lg"
-        >
-          <Box h="85%" mt="md">
-            <Title order={1}>Medium</Title>
-            <Box mx="auto" w="30%" mt="md">
-              <Text fz={50} fw={700}>
-                20
-              </Text>
-              <Text fz={20} fw={700}>
-                euros
-              </Text>
-            </Box>
-            <Box w="30%" mx="auto">
-              <Image src={"/images/medium.png"}></Image>
-            </Box>
-            <Box mt="xl">
-              <Text c="dimmed">Enough for few medium campaigns</Text>
-            </Box>
-          </Box>
-          <Button className={classes.button}>Order Now</Button>
-        </Card>
-        <Card
-          h={PRIMARY_COL_HEIGHT}
-          className={classes.container}
-          ta="center"
-          radius="lg"
-        >
-          <Box h="85%" mt="md">
-            <Title order={1}>Big</Title>
-            <Box mx="auto" w="30%" mt="md">
-              <Text fz={50} fw={700}>
-                50
-              </Text>
-              <Text fz={20} fw={700}>
-                euros
-              </Text>
-            </Box>
-            <Box w="30%" mx="auto">
-              <Image src={"/images/big.png"}></Image>
-            </Box>
-            <Box mt="xl">
-              <Text c="dimmed">Enough for a bigger campaign</Text>
-            </Box>
-          </Box>
-          <Button className={classes.button}>Order Now</Button>
-        </Card>
-        <Card
-          h={PRIMARY_COL_HEIGHT}
-          className={classes.container}
-          ta="center"
-          radius="lg"
-        >
-          <Box h="85%" mt="md">
-            <Title order={1}>Custom</Title>
-            <Box mx="auto" w="60%" mt="md">
-              <Text fz={50} fw={700}>
-                0,5 - 10k
-              </Text>
-              <Text fz={20} fw={700}>
-                euros
-              </Text>
-            </Box>
-
-            <Box w="30%" mx="auto">
-              <Image src={"/images/custom.png"}></Image>
-            </Box>
-
-            <Box mt="xl">
-              <Text c="dimmed">Custom size campaign</Text>
-            </Box>
-          </Box>
-          <Button className={classes.button}>Order Now</Button>
-        </Card>
-
-        {/* <Grid gutter="md">
-          <Grid.Col>
-            <Skeleton
-              height={SECONDARY_COL_HEIGHT}
-              radius="md"
-              animate={false}
-            />
-          </Grid.Col>
-          <Grid.Col span={6}>
-            <Skeleton
-              height={SECONDARY_COL_HEIGHT}
-              radius="md"
-              animate={false}
-            />
-          </Grid.Col>
-          <Grid.Col span={6}>
-            <Skeleton
-              height={SECONDARY_COL_HEIGHT}
-              radius="md"
-              animate={false}
-            />
-          </Grid.Col>
-        </Grid> */}
+        {data && <PricingCards cards={data?.results} />}
       </SimpleGrid>
     </Box>
   );
