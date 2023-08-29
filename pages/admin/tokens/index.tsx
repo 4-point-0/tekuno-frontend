ff11566369f2d67bf83dbc74e59388eda653cf7e
dbosnjak94 <dominik.bosnjak94@gmail.com>
2023-08-29 08:54:41 +0200

    Token page refactor

--- pages/admin/tokens/index.tsx
+++ pages/admin/tokens/index.tsx
@@ -1,38 +1,9 @@
-import {
-  Box,
-  createStyles,
-  SimpleGrid,
-  Title,
-  useMantineTheme,
-} from "@mantine/core";
+import { Box, SimpleGrid, Title } from "@mantine/core";
 
 import PricingCards from "@/components/core/PricingCards/PricingCardsAdmin";
 import { useStripeControllerGetProducts } from "@/services/api/admin/adminComponents";
 
-const useStyles = createStyles((theme) => ({
-  container: {
-    backgroundColor: "white",
-    padding: "2%",
-    margin: "2%",
-    "&:hover": {
-      // backgroundColor: theme.colors.gray[3],
-      boxShadow: "2px 0px 63px -9px rgba(0,0,0,0.51)",
-      transform: "scale(1.05)",
-      transition: "transform 0.3s ease-in-out",
-    },
-  },
-  button: {
-    backgroundColor: "black",
-    "&:hover": {
-      backgroundColor: theme.colors.gray[7],
-    },
-  },
-}));
-
 export const Tokens = () => {
-  const theme = useMantineTheme();
-  const { classes } = useStyles();
-
   const { data } = useStripeControllerGetProducts({});
 
   return (
