6d0468324bb5f6c31a5d5b3ed8dda7d9de76b632
dbosnjak94 <dominik.bosnjak94@gmail.com>
2023-08-31 15:00:35 +0200

    Resolving commit issues

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -4,13 +4,19 @@ import { InferGetServerSidePropsType } from "next";
 import { CallToAction } from "@/components/admin/CallToAction";
 import { getUserServerSideProps } from "@/utils/auth";
 
+type User = {
+  organization_id?: string;
+};
+
 export default function Admin({
   user,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const hasOrganization = Boolean(user?.organization_id);
+
   return (
     <Container fluid sx={{ height: "100%" }}>
       <Paper radius="lg" p="xl" h="100%">
-        <CallToAction hasOrganization={Boolean(user.organization_id)} />
+        <CallToAction hasOrganization={hasOrganization} />
       </Paper>
     </Container>
   );
