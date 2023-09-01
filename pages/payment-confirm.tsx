b2edcea1c9511b4b2ff2ae25d3dd45f5cd2b9bd3
dbosnjak94 <dominik.bosnjak94@gmail.com>
2023-09-01 14:13:58 +0200

    Payment-Confirm page

---
+++ pages/payment-confirm.tsx
@@ -0,0 +1,48 @@
+import {
+  AspectRatio,
+  Box,
+  Center,
+  Container,
+  Image,
+  Text,
+} from "@mantine/core";
+import { useRouter } from "next/router";
+
+import { IndigoButton } from "@/components/core/IndigoButton";
+
+export const PaymentConfirm = () => {
+  const router = useRouter();
+  return (
+    <Container>
+      <Box mt="xl">
+        <Text fz={30} fw={700} ta="center">
+          Thank You for Your Purchase!
+        </Text>
+      </Box>
+      <AspectRatio ratio={720 / 950} maw={400} mx="auto">
+        <Image src={"/images/confirm-payment.png"} radius="xl" />
+      </AspectRatio>
+      <Text fz="lg" ta="center" mt="md">
+        We appreciate your support and hope you enjoy your new tokens.
+      </Text>
+      <Text fz="lg" ta="center" mt="md">
+        If you have any questions or need assistance, feel free to contact our
+        support team.
+      </Text>
+      <Center>
+        <IndigoButton
+          variant="primary"
+          size="lg"
+          mt="xl"
+          onClick={() => {
+            router.push("/");
+          }}
+        >
+          Back to Tekuno
+        </IndigoButton>
+      </Center>
+    </Container>
+  );
+};
+
+export default PaymentConfirm;
