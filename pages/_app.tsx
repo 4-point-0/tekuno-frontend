9f38ee6eb6069876e0d52177b2d0242628723a33
rimatik <slaven@rimatik.hr>
2023-09-22 14:23:26 +0200

    test commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,7 +23,7 @@ export default function App({
   return (
     <>
       <Head>
-        <title>Tekuno</title>
+        <title>Tekuno 123</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
