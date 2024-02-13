64f26d03e276f2944c0142bc545feff7dd91ca0f
dbosnjak94 <dominik.bosnjak94@gmail.com>
2024-02-13 14:50:38 +0100

    Update import statement for network context

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-import { useRamper } from "@/context/RamperContext";
+import { useNetwork } from "@/context/NetworkContext";
 import { useIsClient } from "@/hooks/useIsClient";
 
 export default function Home() {
   const isClient = useIsClient();
   const router = useRouter();
-  const { user } = useRamper();
+  const { user } = useNetwork();
 
   useEffect(() => {
     if (!isClient) {
