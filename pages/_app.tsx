f5bda17378a8fc0cab50ec25033a1a3f5819ae65
dbosnjak94 <dominik.bosnjak94@gmail.com>
2024-02-15 14:40:07 +0100

    Add SuiClientProvider and WalletProvider to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
+"use client";
+
 import { MantineProvider } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
 import { Notifications } from "@mantine/notifications";
+import {
+  createNetworkConfig,
+  SuiClientProvider,
+  WalletProvider,
+} from "@mysten/dapp-kit";
+import "@mysten/dapp-kit/dist/index.css";
+import { getFullnodeUrl } from "@mysten/sui.js/client";
+import { registerZkSendWallet } from "@mysten/zksend";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
@@ -11,6 +21,22 @@ import { AppLayout } from "@/components/layout/AppLayout";
 import { tekunoTheme } from "@/styles/theme";
 
 const queryClient = new QueryClient();
+
+//TODO: implement ZkLogin
+// const ZK_SEND_HOST = window.location.host.includes("localhost")
+//   ? "http://localhost:3000"
+//   : "https://preview.zksend.com";
+
+const { networkConfig } = createNetworkConfig({
+  localnet: { url: getFullnodeUrl("localnet") },
+  devnet: { url: getFullnodeUrl("devnet") },
+  testnet: { url: getFullnodeUrl("testnet") },
+  mainnet: { url: getFullnodeUrl("mainnet") },
+});
+
+// registerZkSendWallet("Tekuno", {
+//   origin: `https://preview.zksend.com`,
+// });
 
 export default function App({
   Component,
@@ -43,10 +69,17 @@ export default function App({
             }
           >
             <ModalsProvider>
-              <Notifications />
-              <AppLayout>
-                <Component {...pageProps} />
-              </AppLayout>
+              <SuiClientProvider
+                networks={networkConfig}
+                defaultNetwork="mainnet"
+              >
+                <WalletProvider autoConnect>
+                  <Notifications />
+                  <AppLayout>
+                    <Component {...pageProps} />
+                  </AppLayout>
+                </WalletProvider>
+              </SuiClientProvider>
             </ModalsProvider>
           </MantineProvider>
         </QueryClientProvider>
